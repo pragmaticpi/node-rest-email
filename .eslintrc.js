d559f82c88b08902c45d646c913ee0d3a28f311b
Piyush Kumar <kpiyush1709@gmail.com>
2018-04-01 23:16:32 +0530

    Add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        'airbnb-base',
+    ],
+    plugins: [
+        'import',
+    ],
+    env: {
+        node: true,
+    },
+    "rules": {
+      "no-console": 0,
+    }
+};
