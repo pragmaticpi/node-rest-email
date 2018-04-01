3df96fdaac0388f509bf10187ea85fb443002723
Piyush Kumar <kpiyush1709@gmail.com>
2018-04-01 23:16:45 +0530

    Changes according to eslint

--- app.js
+++ app.js
@@ -5,35 +5,36 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(bodyParser.json());
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'yourEmail@gmail.com',
-        pass: 'yourPassword'
-    }
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'yourEmail@gmail.com',
+    pass: 'yourPassword',
+  },
 });
 
 app.get('/', (req, res) => {
-    res.send('Email Rest API');
+  res.send('Email Rest API');
 });
 
 app.post('/:emailId/:subject/:body', (req, res) => {
-    const emailId = req.params.emailId;
-    const sub = req.params.subject;
-    const body = req.params.body;
-    let mail = {
-        from: 'yourEmail@gmail.com',
-        to: emailId,
-        subject: sub,
-        text: body,
-    };
-    transporter.sendMail(mail, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Mail sent: ' + info.res);
-        }
-    });
+  const { emailId } = req.params.emailId;
+  const { sub } = req.params.subject;
+  const { body } = req.params.body;
+  const mail = {
+    from: 'yourEmail@gmail.com',
+    to: emailId,
+    subject: sub,
+    text: body,
+  };
+  transporter.sendMail(mail, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send('Mail sent');
+      console.log(`Mail sent: ${info.res}`);
+    }
+  });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 app.listen(3000, () => {
-    console.log('Server started');
+  console.log('Server started');
 });
